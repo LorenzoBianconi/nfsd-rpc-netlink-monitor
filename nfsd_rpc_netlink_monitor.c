5aa5474c1c4bf2003bc0228e3c38cd3d394444b0
Lorenzo Bianconi <lorenzo@kernel.org>
2023-09-07 12:28:10 +0200

    nfsd_rpc_netlink_monitor: update to latest uAPI version

--- nfsd_rpc_netlink_monitor.c
+++ nfsd_rpc_netlink_monitor.c
@@ -15,68 +15,13 @@
 #include <netlink/attr.h>
 #include <linux/netlink.h>
 
+#include "nfsd_server.h"
+
 /* compile note:
  * gcc -I/usr/include/libnl3/ -o <prog-name> <prog-name>.c -lnl-3 -lnl-genl-3
  */
 
-#define SERVER_NAME	"nfsd_server"
 #define BUFFER_SIZE	8192
-
-static volatile bool exiting;
-
-enum nfs_commands {
-	NFS_CMD_UNSPEC,
-
-	NFS_CMD_GET_RPC_STATUS,
-	NFS_CMD_NEW_RPC_STATUS,
-
-	/* add new commands above here */
-
-	__NFS_CMD_MAX,
-	NFS_CMD_MAX = __NFS_CMD_MAX - 1,
-};
-
-enum nfs_rcp_status_compound_attrs {
-	__NFS_ATTR_RPC_STATUS_COMPOUND_INVALID,
-	NFS_ATTR_RPC_STATUS_COMPOUND_OP,
-
-	/* keep it last */
-	NUM_NFS_ATTR_RPC_STATUS_COMPOUND,
-	NFS_ATTR_RPC_STATUS_COMPOUND_MAX = NUM_NFS_ATTR_RPC_STATUS_COMPOUND - 1,
-};
-
-enum nfs_rpc_status_attrs {
-	__NFS_ATTR_RPC_STATUS_INVALID,
-
-	NFS_ATTR_RPC_STATUS_XID,
-	NFS_ATTR_RPC_STATUS_FLAGS,
-	NFS_ATTR_RPC_STATUS_PC_NAME,
-	NFS_ATTR_RPC_STATUS_VERSION,
-	NFS_ATTR_RPC_STATUS_STIME,
-	NFS_ATTR_RPC_STATUS_SADDR4,
-	NFS_ATTR_RPC_STATUS_DADDR4,
-	NFS_ATTR_RPC_STATUS_SADDR6,
-	NFS_ATTR_RPC_STATUS_DADDR6,
-	NFS_ATTR_RPC_STATUS_SPORT,
-	NFS_ATTR_RPC_STATUS_DPORT,
-	NFS_ATTR_RPC_STATUS_PAD,
-	NFS_ATTR_RPC_STATUS_COMPOUND,
-
-	/* keep it last */
-	NUM_NFS_ATTR_RPC_STATUS,
-	NFS_ATTR_RPC_STATUS_MAX = NUM_NFS_ATTR_RPC_STATUS - 1,
-};
-
-enum nfs_attrs {
-	NFS_ATTR_UNSPEC,
-
-	NFS_ATTR_RPC_STATUS,
-
-	/* add new attributes above here */
-
-	__NFS_ATTR_MAX,
-	NFS_ATTR_MAX = __NFS_ATTR_MAX - 1
-};
 
 static int error_handler(struct sockaddr_nl *nla, struct nlmsgerr *err,
 			 void *arg)
@@ -106,86 +51,67 @@ static int ack_handler(struct nl_msg *msg, void *arg)
 static int recv_handler(struct nl_msg *msg, void *arg)
 {
 	struct genlmsghdr *gnlh = nlmsg_data(nlmsg_hdr(msg));
-	struct nlattr *tb[NFS_ATTR_MAX + 1], *rpc_attr;
-	int m;
+	struct nlattr *tb[NFSD_ATTR_RPC_STATUS_MAX + 1];
 
-	nla_parse(tb, NFS_ATTR_MAX, genlmsg_attrdata(gnlh, 0),
+	nla_parse(tb, NFSD_ATTR_RPC_STATUS_MAX, genlmsg_attrdata(gnlh, 0),
 		  genlmsg_attrlen(gnlh, 0), NULL);
 
-	if (!tb[NFS_ATTR_RPC_STATUS]) {
-		fprintf(stderr, "rpc status attr missing\n");
-		return NL_SKIP;
+	if (tb[NFSD_ATTR_RPC_STATUS_XID])
+		printf(" 0x%08x", nla_get_u32(tb[NFSD_ATTR_RPC_STATUS_XID]));
+
+	if (tb[NFSD_ATTR_RPC_STATUS_FLAGS])
+		printf(" 0x%08x", nla_get_u32(tb[NFSD_ATTR_RPC_STATUS_FLAGS]));
+
+	if (tb[NFSD_ATTR_RPC_STATUS_PC_NAME])
+		printf(" %s", nla_get_string(tb[NFSD_ATTR_RPC_STATUS_PC_NAME]));
+
+	if (tb[NFSD_ATTR_RPC_STATUS_VERSION])
+		printf(" NFS%d", nla_get_u8(tb[NFSD_ATTR_RPC_STATUS_VERSION]));
+
+	if (tb[NFSD_ATTR_RPC_STATUS_SERVICE_TIME])
+		printf(" %ld",
+		       nla_get_u64(tb[NFSD_ATTR_RPC_STATUS_SERVICE_TIME]));
+
+	if (tb[NFSD_ATTR_RPC_STATUS_SADDR4]) {
+		struct in_addr addr = {
+			.s_addr = nla_get_u32(tb[NFSD_ATTR_RPC_STATUS_SADDR4])
+		};
+
+		printf(" %s", inet_ntoa(addr));
 	}
 
-	nla_for_each_nested(rpc_attr, tb[NFS_ATTR_RPC_STATUS], m) {
-		struct nlattr *rq_attr[NFS_ATTR_RPC_STATUS_MAX + 1];
+	if (tb[NFSD_ATTR_RPC_STATUS_SPORT])
+		printf(" %hu", nla_get_u16(tb[NFSD_ATTR_RPC_STATUS_SPORT]));
 
-		nla_parse_nested(rq_attr, NFS_ATTR_RPC_STATUS_MAX, rpc_attr,
-				 NULL);
-		if (rq_attr[NFS_ATTR_RPC_STATUS_XID])
-			printf(" 0x%08x",
-			       nla_get_u32(rq_attr[NFS_ATTR_RPC_STATUS_XID]));
+	if (tb[NFSD_ATTR_RPC_STATUS_DADDR4]) {
+		struct in_addr addr = {
+			.s_addr = nla_get_u32(tb[NFSD_ATTR_RPC_STATUS_DADDR4])
+		};
 
-		if (rq_attr[NFS_ATTR_RPC_STATUS_FLAGS])
-			printf(" 0x%08x",
-			       nla_get_u32(rq_attr[NFS_ATTR_RPC_STATUS_FLAGS]));
+		printf(" %s", inet_ntoa(addr));
+	}
 
-		if (rq_attr[NFS_ATTR_RPC_STATUS_PC_NAME])
+	if (tb[NFSD_ATTR_RPC_STATUS_DPORT])
+		printf(" %hu", nla_get_u16(tb[NFSD_ATTR_RPC_STATUS_DPORT]));
+
+	if (tb[NFSD_ATTR_RPC_STATUS_COMPOND_OP]) {
+		struct nlattr *op_attr[NFSD_ATTR_RPC_STATUS_COMP_MAX + 1];
+		struct nlattr *attr;
+		int m;
+
+		nla_for_each_nested(attr,
+				    tb[NFSD_ATTR_RPC_STATUS_COMPOND_OP], m) {
+			nla_parse_nested(op_attr,
+					 NFSD_ATTR_RPC_STATUS_COMP_MAX, attr,
+					 NULL);
+			if (!op_attr[NFSD_ATTR_RPC_STATUS_COMP_OP])
+				continue;
+
 			printf(" %s",
-			       nla_get_string(rq_attr[NFS_ATTR_RPC_STATUS_PC_NAME]));
-
-		if (rq_attr[NFS_ATTR_RPC_STATUS_VERSION])
-			printf(" NFS%d",
-			       nla_get_u8(rq_attr[NFS_ATTR_RPC_STATUS_VERSION]));
-
-		if (rq_attr[NFS_ATTR_RPC_STATUS_STIME])
-			printf(" %ld",
-			       nla_get_u64(rq_attr[NFS_ATTR_RPC_STATUS_STIME]));
-
-		if (rq_attr[NFS_ATTR_RPC_STATUS_SADDR4]) {
-			struct in_addr addr = {
-				.s_addr = nla_get_u32(rq_attr[NFS_ATTR_RPC_STATUS_SADDR4])
-			};
-
-			printf(" %s", inet_ntoa(addr));
+			       nla_get_string(op_attr[NFSD_ATTR_RPC_STATUS_COMP_OP]));
 		}
-
-		if (rq_attr[NFS_ATTR_RPC_STATUS_SPORT])
-			printf(" %hu", nla_get_u16(rq_attr[NFS_ATTR_RPC_STATUS_SPORT]));
-
-		if (rq_attr[NFS_ATTR_RPC_STATUS_DADDR4]) {
-			struct in_addr addr = {
-				.s_addr = nla_get_u32(rq_attr[NFS_ATTR_RPC_STATUS_DADDR4])
-			};
-
-			printf(" %s", inet_ntoa(addr));
-		}
-
-		if (rq_attr[NFS_ATTR_RPC_STATUS_DPORT])
-			printf(" %hu", nla_get_u16(rq_attr[NFS_ATTR_RPC_STATUS_DPORT]));
-
-		if (rq_attr[NFS_ATTR_RPC_STATUS_COMPOUND]) {
-			struct nlattr *comp_attr;
-			int n;
-
-			nla_for_each_nested(comp_attr,
-					    rq_attr[NFS_ATTR_RPC_STATUS_COMPOUND],
-					    n) {
-				struct nlattr *op_attr[NFS_ATTR_RPC_STATUS_COMPOUND_MAX + 1];
-
-				nla_parse_nested(op_attr,
-						 NFS_ATTR_RPC_STATUS_COMPOUND_MAX,
-						 comp_attr, NULL);
-				if (!op_attr[NFS_ATTR_RPC_STATUS_COMPOUND_OP])
-					continue;
-
-				printf(" %s",
-				       nla_get_string(op_attr[NFS_ATTR_RPC_STATUS_COMPOUND_OP]));
-			}
-		}
-
-		printf("\n");
 	}
+	printf("\n");
 
 	return NL_SKIP;
 }
@@ -211,9 +137,9 @@ int main(char argc, char **argv)
 	setsockopt(nl_socket_get_fd(sock), SOL_NETLINK, NETLINK_EXT_ACK,
 		   &ret, sizeof(ret));
 
-	id = genl_ctrl_resolve(sock, SERVER_NAME);
+	id = genl_ctrl_resolve(sock, NFSD_SERVER_FAMILY_NAME);
 	if (id < 0) {
-		fprintf(stderr, "%s not found\n", SERVER_NAME);
+		fprintf(stderr, "%s not found\n", NFSD_SERVER_FAMILY_NAME);
 		ret = -ENOENT;
 		goto out;
 	}
@@ -232,7 +158,7 @@ int main(char argc, char **argv)
 		goto out;
 	}
 
-	genlmsg_put(msg, 0, 0, id, 0, 0, NFS_CMD_GET_RPC_STATUS, 0);
+	genlmsg_put(msg, 0, 0, id, 0, NLM_F_DUMP, NFSD_CMD_RPC_STATUS_GET, 0);
 
 	ret = nl_send_auto_complete(sock, msg);
 	if (ret < 0)
